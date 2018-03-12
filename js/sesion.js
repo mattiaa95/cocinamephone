5beb1d8a5460d525ef0945d459f7bdac417fbea1
Mattia La Spina <mat_halo_95@hotmail.com>
2018-03-12 16:14:49 +0000

    subvo

--- js/sesion.js
+++ js/sesion.js
@@ -11,26 +11,29 @@ console.log("sesion ready");
           data: formData,
           contentType: false,
           processData: false,
-        success: function(respuesta){
-          alert(respuesta);
-            var objetojs = jQuery.parseJSON(JSON.stringify(respuesta));
-            alert(objetojs);
-            if (objetojs.estado == 1) {
-              window.sessionStorage.setItem('id_usuario', objetojs.id);
-              window.sessionStorage.setItem('password', objetojs.password);
-              window.sessionStorage.setItem('usuario', objetojs.usuario);
-              window.sessionStorage.setItem('nombre', objetojs.nombre);
-              window.sessionStorage.setItem('apellidos', objetojs.apellidos);
-              window.sessionStorage.setItem('descripcion', objetojs.descripcion);
-              window.sessionStorage.setItem('fecha', objetojs.fecha);
-              window.sessionStorage.setItem('email', objetojs.email);
-              window.document.location.href = "index.html";
-            }
+        success: function(){
+            alert("funciono");
           },
           error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert("Status: " + textStatus); alert("Error: " + errorThrown);
           }
 
-          })
+          }).done(function(respuesta){
+            alert(respuesta);
+              var objetojs = jQuery.parseJSON(JSON.stringify(respuesta));
+              alert(objetojs);
+              if (objetojs.estado == 1) {
+                window.sessionStorage.setItem('id_usuario', objetojs.id);
+                window.sessionStorage.setItem('password', objetojs.password);
+                window.sessionStorage.setItem('usuario', objetojs.usuario);
+                window.sessionStorage.setItem('nombre', objetojs.nombre);
+                window.sessionStorage.setItem('apellidos', objetojs.apellidos);
+                window.sessionStorage.setItem('descripcion', objetojs.descripcion);
+                window.sessionStorage.setItem('fecha', objetojs.fecha);
+                window.sessionStorage.setItem('email', objetojs.email);
+                window.document.location.href = "index.html";
+              }
+
+  				});
     });
   });
