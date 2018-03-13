761bc9125917e103152bcef56c664961f38e9abf
Mattia La Spina <mat_halo_95@hotmail.com>
2018-03-13 15:52:18 +0000

    d
    sd

--- js/perfil.js
+++ js/perfil.js
@@ -1,8 +1,15 @@
 $( document ).ready(function() {
-  $("#pr-imagen").attr("src","http://10.10.100.13/img/"+window.sessionStorage.getItem('imagen'));
-  $("#pr-nombre").text(window.sessionStorage.getItem('nombre'));
-  $("#pr-nomap").text(window.sessionStorage.getItem('nombre')+" "+sessionStorage.getItem('apellidos'));
-  $("#pr-fecha").text(window.sessionStorage.getItem('fecha'));
-  $("#pr-descripcion").text(window.sessionStorage.getItem('descripcion'));
-  $("#pr-email").text(window.sessionStorage.getItem('email'));
+
+  if (window.sessionStorage.getItem('usuario')) {
+    $("#pr-imagen").attr("src","http://10.10.100.13/img/"+window.sessionStorage.getItem('imagen'));
+    $("#pr-nombre").text(window.sessionStorage.getItem('nombre'));
+    $("#pr-nomap").text(window.sessionStorage.getItem('nombre')+" "+sessionStorage.getItem('apellidos'));
+    $("#pr-fecha").text(window.sessionStorage.getItem('fecha'));
+    $("#pr-descripcion").text(window.sessionStorage.getItem('descripcion'));
+    $("#pr-email").text(window.sessionStorage.getItem('email'));
+  }else {
+  window.document.location.href = "index.html";
+  }
+
+
 });
